6e77fae2e4656cf43f367c99c53595458b04a79e
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-07-12 23:36:24 +0000

    Updating

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let generalDiv = document.querySelector(".container")
+let html_question = document.createElement("div")
+generalDiv.append(html_question)
+html_question.setAttribute("class", "html_question")
+
+fetch("../json/quiz.json")
+    .then(res => res.json())
+    .then(data => {
+        data.questions.forEach((ques, ind) => {
+            let opt = ques.options
+            let options = "";
+            opt.forEach((ele) => {
+                options += `
+                    <li>
+                        <label for="${ques.number}" name="${ques.number}" id="${ques.number}${ind}">
+                            <input type="radio" class="option" name="${ques.number}" data-ben="${ques.answer}" value="${ele}">${ele}</input>
+                        </label>
+                    </li>`
+            })
+            html_question.innerHTML += `
+            <div class="question">
+                <div> <b> ${ques.number}. ${ques.question}</b></div>
+                ${options}
+             </div>
+            `
+        });
+        document.querySelectorAll(".option").forEach(elem => elem.addEventListener("change", (e) => {
+                console.log(e.target.dataset.ben)
+                e.target.parentElement.style.backgroundColor = (e.target.value == e.target.dataset.ben && e.target.checked == true) ? "green" : "none"
+            })
+
+        )
+
+
+        // console.log(.id)
+        // answer.lastChild.data
+        // console.log(answer)
+        // && e.target.value == ans.includes(e.target.value)
+        // .insertAdjacentHTML("beforebegin", )
+    })
